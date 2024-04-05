8681c0b363f5e55456a9c023a2a5f78ef0282109
Kahle <kahle@undefined>
2024-04-06 06:34:40 +0800

    版本1

--- src/main/java/sagiri/Application.java
+++ src/main/java/sagiri/Application.java
@@ -4,11 +4,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * Startup class.
  * @author Kahle
  */
+@EnableAsync
+@EnableScheduling
 @SpringBootApplication
 public class Application {
     private static Logger log = LoggerFactory.getLogger(Application.class);
